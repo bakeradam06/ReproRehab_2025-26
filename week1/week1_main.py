b949f4f0da453f11b07410acc0f887dddf32a390
Adam Baker <bakerdon@musc.edu>
2025-10-03 12:49:09 -0400

    Update week1_main.py

--- week1/week1_main.py
+++ week1/week1_main.py
@@ -4,19 +4,26 @@
 print("Hello, World!")
 
 
-
-
 # real exercises, starting at 4. 1-3 are in the m file within this folder: 
+
 
 # prompt 4:
 # List Comprehensions (Python only):
 # Create a list of squares of numbers from 1 to 10
 # Given [[1,A], [2,B], [3,C]] return [[1,2,3], [A,B,C]]
 
+# part 1
+squares = [n**2 for n in range (1,11)] # list of squares from 1 to 10. keep in mind that i needed to include 11 here instead of stopping at 10, since python prefers this method. 
+print(squares) # print results
 
+# part 2
+a = 'a'
+b = 'b'
+c = 'c'
 
-
-
+let_and_num = [[1,a], [2,b], [3,c]]
+y = [[item[0] for item in let_and_num],[item[1] for item in let_and_num]] # python starts indexing at 0, remember that! here, within brackets, you're printing item 0 in the index of item inside let_and_num, and then doing the same for item 1, which is the second entry. 
+print(y)
 
 # prompt 5:
 # Dict Comprehensions (Python only):
