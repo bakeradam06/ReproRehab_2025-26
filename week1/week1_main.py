9f42fd1ca2aff4b9e233f0b37966661fc359ad5f
Adam Baker <bakerdon@musc.edu>
2025-10-03 12:14:59 -0400

    Create week1_main.py

---
+++ week1/week1_main.py
@@ -0,0 +1,37 @@
+### Week 1 - Python exercises for ReproRehab week 1 assignment
+
+# fun, not actually exercise 1: Hello, World!
+print("Hello, World!")
+
+
+
+
+# real exercises, starting at 4. 1-3 are in the m file within this folder: 
+
+# prompt 4:
+# List Comprehensions (Python only):
+# Create a list of squares of numbers from 1 to 10
+# Given [[1,A], [2,B], [3,C]] return [[1,2,3], [A,B,C]]
+
+
+
+
+
+
+# prompt 5:
+# Dict Comprehensions (Python only):
+# Create a dictionary mapping numbers 1-5 to their squares using key/value pairings
+# Create a dictionary of of numbers 1-3, where each value is another dictionary mapping the number to its square and cube
+# Answer: {1:{“sqaure”: 1; “cube”: 1}}
+
+
+
+
+
+
+# prompt 6
+# Sets (Python only):
+# Turn a list [1, 2, 2, 3, 4, 4, 5] into {1, 2, 3, 4, 5} using a set
+# Given a set {1, 2, 3}, check if 2 and 5 are in it
+# From a list [1, 2, 3], generate all unique unordered pairs {(1, 2), (1, 3), (2, 3)} using sets.
+
