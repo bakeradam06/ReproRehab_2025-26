fc36496a9ce62b0c46c0ba1cb0c7bc895f2dd766
Adam Baker <bakerdon@musc.edu>
2025-10-08 09:35:47 -0400

    Update week2_main.m
    added prompts for week

--- week2/week2_main.m
+++ week2/week2_main.m
@@ -1,1 +1,44 @@
 % week 2 main
+
+%% Creating some data
+% Create a folder with name "Data"
+
+
+% Within the "Data" folder, add two folders: "Subject 1" & "Subject 2"
+
+
+% Within each "Subject X", add two files: data.csv and info.txt
+
+
+% Add three numbers, separated by commas in each data.csv (e.g., 1,2,3)
+
+
+% Add name to info.txt (e.g., Dan)
+
+
+% Make sure numbers and names are unique to subject
+
+
+
+
+
+
+
+%% Script
+% Write a loop that iterates through all folders in the "Data" folder
+
+
+% Write a nested loop that iterates through all "Subject X" folders
+
+
+% Write code to load the data from data.csv and info.txt and print the results
+
+
+% Store the data as a dict (Python) / struct (MATLAB), where data from info is key and data from data is value (e.g., Dan: [1,2,3])
+
+
+% Add another subject folder and verify that everything works and new data is added. 
+    % The goal here is to not have to modify the script every time a new subject is added
+
+
+% MATLAB only: pre-allocate an array to hold your data
