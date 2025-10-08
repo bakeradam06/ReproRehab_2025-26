4eac2c85b02b6ae4cfea7502712ada9ab2c1aaa7
Adam Baker <bakerdon@musc.edu>
2025-10-08 09:40:16 -0400

    added week3 folder & prompts

---
+++ week3/week3_main.m
@@ -0,0 +1,48 @@
+% week 3
+
+ 
+%% Data
+% Create a variable called "Control" and set to 10 random values uniformly distributed between 0 and 3 
+
+
+% Create another variable called "Experimental" and set to 10 random values uniformly distributed between 5 and 9
+
+%% Visualize
+% Create a plot to visualize the comparison between "Control" and "Experimental" groups
+
+
+% Add x- and y-labels to the figure 
+
+
+% Add labels to x ticks (Control and Experimental)
+
+
+% Add a title
+
+
+% Remove axis lines on top and right side
+
+% Set x and y label text size to 28
+
+% Set x and y tick text size to 12
+
+% Set title text size to 30
+
+% Make sure legend text is same as tick text size
+
+% Show that the two groups are significantly different 
+    % MATLAB - sigstar
+    % Python - YOYO (your own your own)
+ 
+%% Saving
+% Save figure in publication format (.pdf)
+
+
+% dpi = 300 for Python
+
+%% Bonus / Shameless Plug / Flex 
+% Save figure in folder of current date/time to stay organized
+    % Matlab: https://tulimid1.github.io/savingfigures/MATLAB/
+    % Python: https://tulimid1.github.io/savingfigures/Python/
+
+% Put figure making process into a loop and iterate three times to get three publication ready figures
