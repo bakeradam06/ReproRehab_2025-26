a3715e4847ab4a87180198812826e3a72dbbae69
Adam Baker <bakerdon@musc.edu>
2025-11-21 09:13:15 -0500

    add wk 8 exercises

---
+++ week8/week8_main.m
@@ -0,0 +1,35 @@
+%% week 8 main
+ 
+
+
+
+ 
+%% Data
+    % 1.	Generate two sets of data (2D matrices) that will represent the control and test groups walking speed 
+        % a.	The control group has 21 participants and completed 100 trials each
+            % i.	Generate with normal random, mean of 7, standard deviation of 1.2
+        % b.	The test group has 17 participants and completed 100 trials each 
+            % i.	Generate with normal random, mean of 3, standard deviation of 5
+
+            
+
+%% Summarize Data
+
+
+% 1.	Calculate the average walking speed per participant (across trials) for each group
+% 2.	Calculate the standard deviation of walking speed per participant (across trials) for each group
+% 3.	Calculate the average walking speed per trial (across participants) for each group
+% 4.	Calculate the standard deviation of walking speed per trial (across participants) for each group
+
+
+
+
+%% Normality
+% 1.	Check to see if the average walking speed per group passes a normality test (Kruski-Wallis?)
+
+
+
+
+
+%% Comparison
+% 1.	Run a t-test (or non-parametric equivalent if wanted / needed) comparing the average walking speed between groups
