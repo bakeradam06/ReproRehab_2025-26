7dee76984ef3e732332910ec24659a490dc76f58
Adam Baker <bakerdon@musc.edu>
2025-12-05 14:36:40 -0500

    update write function call within subject folders

--- week2/week2_main.m
+++ week2/week2_main.m
@@ -5,58 +5,54 @@
 % Create a folder with name "Data"
 mkdir '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
 mainPath = '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
-% Within the "Data" folder, add two folders: "Subject 1" & "Subject 2"
+cd(mainPath)
 
-% nav to folder
-cd('/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data')
-% make new folders per subject
+% Within the "Data" folder, add two folders: "Subject 1" & "Subject 2"
 mkdir subject1
 mkdir subject2
 
-% below for rest of section: i had to use matlab copilot to help me with this.
-% i heavily document these tasks to ensure i understand all that is going on in the code.
+% for rest of section: i had to use matlab copilot to help me with this. primarily the command for making isSubj variable.
+% i explain these tasks in comments to ensure i understand all that's happening.
 
 temp_nSubj = dir(fullfile(pwd,'*')); % create struct, and struct.name contains list of subjects plus two extra entries ('.' and '..')
-isSubj = [nSubj.isdir] & ~ismember({temp_nSubj.name},{'.','..'}); % get logical array of which entry includes subject names, exclude the dots
+isSubj = [temp_nSubj.isdir] & ~ismember({temp_nSubj.name},{'.','..'}); % get logical array of which entry includes subject names, exclude the dots
 nSubj = sum(isSubj); % return count of how many subjects present as "1" in above logical array (2). 
-
 
 %% Within each "Subject X", add two files: data.csv and info.txt
 % cd subject1
 data = [1,2,3,4,5];
-testInfo = "this is a test file for ReproRehab";
+testInfo = "this is a test file for ReproRehab week2 assignment";
 
-% add files
+% add files, and a few numbers, separated by commas in each data.csv (e.g., 1,2,3)
+
+
 cd subject1/ % get to subject1 folder 
-writematrix(data, 'data.csv')
-writelines(testInfo,'info.txt')
+write(data, testInfo);
 cd ..
+
 cd subject2/ % with this and above line, get to subject2
-writematrix(data, 'data.csv')
-writelines(testInfo,'info.txt') 
-
+write(data, testInfo);
 cd ..
 
-%% Add three numbers, separated by commas in each data.csv (e.g., 1,2,3)
+% --------------- % 
+% create function for above steps
+function write(data, testInfo)
+    writematrix(data, 'data.csv');
+    writelines(testInfo, 'info.txt');
+end
+% --------------- % 
 
 
 
-
-% %%
-% for i=1:length()
-
-% pathStruct = dir(basePath); % create struct containing contents of /Data
+%% create struct containing contents of /Data
+pathStruct = dir(mainPath); 
 
 
-% 
-% 
-% 
-% %% 
-% % error here:
-% subjDirs = contents([pathStruct.isdir] & startsWith({pathStruct.name},'subject')); % get contents from struct that are (1) isDir and (2) starts with 
-%     % subject, as noted in the struct. Returns new struct.
-% % construct both subject paths, so then we can loop through them and do operations
-% subjPaths = fullfile(basePath,{subjDirs.name});
+% get contents from struct that are (1) isDir and (2) starts with 
+subjDirs = contents([pathStruct.isdir] & startsWith({pathStruct.name},'subject')); 
+    % subject, as noted in the struct. Returns new struct.
+% construct both subject paths, so then we can loop through them and do operations
+subjPaths = fullfile(basePath,{subjDirs.name});
 
 %% use subjPaths contents to loop through and complete operation
 for i=1:length(subjPaths)
@@ -94,11 +90,11 @@ end
 % Write a nested loop that iterates through all "Subject X" folders
 cd /Data
 
-for i:length(dirContent)
-    for 
-        
-    end
-end
+% for i:length(dirContent)
+%     for 
+% 
+%     end
+% end
 
 
 % Write code to load the data from data.csv and info.txt and print the results
