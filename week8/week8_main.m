c8ddee0787d82a2275d5b3438ec0f57e954f4f82
Adam Baker <bakerdon@musc.edu>
2025-12-08 13:43:48 -0500

    updated normality check - hist
    also cleaned up script

--- week8/week8_main.m
+++ week8/week8_main.m
@@ -2,26 +2,24 @@
  
  
 %% Data
-    % 1.	Generate two sets of data (2D matrices) that will represent the control and test groups walking speed
-        % a.	The control group has 21 participants and completed 100 trials each
+% 1.	Generate two sets of data (2D matrices) that will represent the control and test groups walking speed
+    % a.	The control group has 21 participants and completed 100 trials each
             control = zeros(100,21);
             treat = zeros(100,21);
 
-            % i.	Generate with normal random, mean of 7, standard deviation of 1.2
+        % i.	Generate with normal random, mean of 7, standard deviation of 1.2
             muControl = 7;
             sigmaControl = 1.2;
             control = sigmaControl.*randn(100,21) + muControl; 
 
-        % b.	The test group has 17 participants and completed 100 trials each 
-            % i.	Generate with normal random, mean of 3, standard deviation of 5
+    % b.	The test group has 17 participants and completed 100 trials each 
+        % i.	Generate with normal random, mean of 3, standard deviation of 5
 
             muTreat = 3;
             sigmaTreat = 5;
             treat = sigmaTreat.*randn(100,21) + muTreat;
-
-% note: used the matlab help section to generate this code. I didn't know
-% how to generate an array with specific meawn or sd. now i do!
-
+            % ref: matlab help section. I didn't know
+            % how to generate an array with specific meawn or sd. now i do.
 
            % check: 
            x1 = mean(mean(control(:,:))); 
@@ -31,7 +29,7 @@
            x = mean(mean(treat(:,:))); % returns mean of means per column
            y = mean(std(treat)); % returns mean of stdev per column
 
-%% Summarize Data
+%% Summarize
 
 % 1.	Calculate the average walking speed per participant (across trials) for each group
         meanByPtCntrl = [ ];
@@ -47,9 +45,6 @@
         end
 
         % returns 21x1 array of means by column (avging across trials by participant)
-
-
-        % no help on this section, juist trial and error
 
 %% 2.	Calculate the standard deviation of walking speed per participant (across trials) for each group
         stdByPtCntrl = [ ];
@@ -73,15 +68,18 @@
         sdControl = std(control);
         sdTreat = std(treat); % returns 1x21 array of sd's
 
-
 %% Normality
 % 1.	Check to see if the average walking speed per group passes a normality test (Kruski-Wallis?)
         
         normCntrl = kruskalwallis(control);
         normTreat = kruskalwallis(treat);
 
-        % result: yes, both pass normality check.
+        % check visually
+        histogram(control,15,'FaceColor','r')
+        hold on
+        histogram(treat,15,'FaceColor','b')
 
+        % result: only treatment group is normal. control is abnormal.
 
 %% Comparison
 % 1.	Run a t-test (or non-parametric equivalent if wanted / needed) comparing the average walking speed between groups
