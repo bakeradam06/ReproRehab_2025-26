f49b3e2082bd90f1d336b26372a664f40e58e13f
Adam Baker <bakerdon@musc.edu>
2025-12-08 14:22:33 -0500

    update week2 format
    also add notes for week6 and week7 lack of activities

--- week2/week2_main.m
+++ week2/week2_main.m
@@ -3,36 +3,35 @@
 
 %% Creating some data
 % Create a folder with name "Data"
-mkdir '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
-mainPath = '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
-cd(mainPath)
+    mkdir '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
+    mainPath = '/Users/DOB223/Library/CloudStorage/OneDrive-MedicalUniversityofSouthCarolina/Documents/lab/ac/ReproRehab/bootCamp/2week/Data';
+    cd(mainPath)
 
 % Within the "Data" folder, add two folders: "Subject 1" & "Subject 2"
-mkdir subject1
-mkdir subject2
+    mkdir subject1
+    mkdir subject2
+    
+    % for rest of section: i had to use matlab copilot to help me with this. primarily the command for making isSubj variable.
+    % i explain these tasks in comments to ensure i understand all that's happening.
 
-% for rest of section: i had to use matlab copilot to help me with this. primarily the command for making isSubj variable.
-% i explain these tasks in comments to ensure i understand all that's happening.
-
-temp_nSubj = dir(fullfile(pwd,'*')); % create struct, and struct.name contains list of subjects plus two extra entries ('.' and '..')
-isSubj = [temp_nSubj.isdir] & ~ismember({temp_nSubj.name},{'.','..'}); % get logical array of which entry includes subject names, exclude the dots
-nSubj = sum(isSubj); % return count of how many subjects present as "1" in above logical array (2). 
+    temp_nSubj = dir(fullfile(pwd,'*')); % create struct, and struct.name contains list of subjects plus two extra entries ('.' and '..')
+    isSubj = [temp_nSubj.isdir] & ~ismember({temp_nSubj.name},{'.','..'}); % get logical array of which entry includes subject names, exclude the dots
+    nSubj = sum(isSubj); % return count of how many subjects present as "1" in above logical array (2). 
 
 %% Within each "Subject X", add two files: data.csv and info.txt
 % cd subject1
-data = [1,2,3,4,5];
-testInfo = "this is a test file for ReproRehab week2 assignment";
+    data = [1,2,3,4,5];
+    testInfo = "this is a test file for ReproRehab week2 assignment";
 
 % add files, and a few numbers, separated by commas in each data.csv (e.g., 1,2,3)
 
-
-cd subject1/ % get to subject1 folder 
-write(data, testInfo);
-cd ..
-
-cd subject2/ % with this and above line, get to subject2
-write(data, testInfo);
-cd ..
+    cd subject1/ % get to subject1 folder 
+    write(data, testInfo);
+    cd ..
+    
+    cd subject2/ % with this and above line, get to subject2
+    write(data, testInfo);
+    cd ..
 
 % --------------- % 
 % create function for above steps, reduces redundancy
@@ -42,10 +41,8 @@ function write(data, testInfo)
 end
 % --------------- % 
 
-
 %% create struct containing contents of /Data
 pathStruct = dir(mainPath); 
-
 
 % GOAL: get contents from struct that are (1) isDir and (2) starts with 
 % note: i had to have matlab copilot help me with this section. 
@@ -54,54 +51,53 @@ pathStruct = dir(mainPath);
 % struggle with - this task made me realize this.
 
 
-
-% make array of names within struct
-names = string({pathStruct.name});
-% make mask of names i actually want (the subject folders)
-namesMask = startsWith(names, 'subject','IgnoreCase',true); % returns logical vector whether names entry startsWith "subject"
-
-names = names(namesMask); % remove the two dot entries from the names array using the namesMask
-
-clear namesMask nSubj temp_nSubj testinfo data
+    % make array of names within struct
+    names = string({pathStruct.name});
+    % make mask of names i actually want (the subject folders)
+    namesMask = startsWith(names, 'subject','IgnoreCase',true); % returns logical vector whether names entry startsWith "subject"
+    
+    names = names(namesMask); % remove the two dot entries from the names array using the namesMask
+    
+    clear namesMask nSubj temp_nSubj testinfo data
 
 %%
 % construct both subject paths, so then we can loop through them and do operations
-subjPaths = fullfile(mainPath,names);
-
-name1 = "Harry";
-name2 = "Potter";
+    subjPaths = fullfile(mainPath,names);
+    
+    name1 = "Harry";
+    name2 = "Potter";
 % use subjPaths contents to loop through and complete operation
-for i=1:length(subjPaths)
-    cd(subjPaths{i}) % nav to subject folder. use i to index value of subjPaths cell array
-
-    % Add name to info.txt (e.g., Dan)
-    txtFile = readlines('info.txt');
-    dataFile = readmatrix('data.csv');
-    if i == 1
-        txtFile = name1;
-        dataFile = [1 5 10 1 2575];
-    else 
-        txtFile = name2;
-        dataFile = [2575 1 10 5 1]; % oppppsite of above so they're unique from each other
+    for i=1:length(subjPaths)
+        cd(subjPaths{i}) % nav to subject folder. use i to index value of subjPaths cell array
+    
+        % Add name to info.txt (e.g., Dan)
+        txtFile = readlines('info.txt');
+        dataFile = readmatrix('data.csv');
+        if i == 1
+            txtFile = name1;
+            dataFile = [1 5 10 1 2575];
+        else 
+            txtFile = name2;
+            dataFile = [2575 1 10 5 1]; % oppppsite of above so they're unique from each other
+        end
+        writelines(txtFile,'info.txt');
+        writematrix(dataFile,'data.csv');
     end
-    writelines(txtFile,'info.txt');
-    writematrix(dataFile,'data.csv');
-end
 
 % Make sure numbers and names are unique to subject
 
-cd ..
+    cd ..
 
 %% Write a loop that iterates through all folders in the "Data" folder
-
-txtFile2 = 'I am looping through all folders inside /Data dir';
-
-for i=1:length(subjPaths)
-    cd(subjPaths(i));
-    writelines(txtFile2, 'info2.txt');
-    cd ..
-end
     
+    txtFile2 = 'I am looping through all folders inside /Data dir';
+    
+    for i=1:length(subjPaths)
+        cd(subjPaths(i));
+        writelines(txtFile2, 'info2.txt');
+        cd ..
+    end
+        
 %% Write a nested loop that iterates through all "Subject X" folders
 
     % to first do this, i'll make some new folders within each subject dir
@@ -149,23 +145,23 @@ end
 
 % MATLAB only: pre-allocate an array to hold your data
 
-dataStruct = struct('key',zeros(1:2),'value',zeros(2,5)); % make struct, preallocate name field and data
-tempKey = [];
-tempData = [];
+    dataStruct = struct('key',zeros(1:2),'value',zeros(2,5)); % make struct, preallocate name field and data
+    tempKey = [];
+    tempData = [];
 
-    for i=1:length(subjPaths)
-        cd(subjPaths(i));
-        tempKey = readlines('info.txt');
-        tempValue = readmatrix('data.csv'); % load tempValue 
+        for i=1:length(subjPaths)
+            cd(subjPaths(i));
+            tempKey = readlines('info.txt');
+            tempValue = readmatrix('data.csv'); % load tempValue 
 
 
 % Store the data as a dict (Python) / struct (MATLAB), where data from info is key and data 
 % from data is value (e.g., Dan: [1,2,3])
-
-        dataStruct(i).key = tempKey{1}; % get only first entry from 2x1 str array. add to key of struct
-        dataStruct(i).value = tempValue; % add to value of struct
-        cd ..
-    end
+    
+            dataStruct(i).key = tempKey{1}; % get only first entry from 2x1 str array. add to key of struct
+            dataStruct(i).value = tempValue; % add to value of struct
+            cd ..
+        end
 
 
 %% Add another subject folder and verify that everything works when new data are added. 
